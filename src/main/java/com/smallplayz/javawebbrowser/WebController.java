8a485ad45ac8ae50815fd024c112a3399240d8d5
smallplayz <900raiyan@gmail.com>
2023-04-13 09:22:46 -0400

    more stuff fixxed etc.

--- src/main/java/com/smallplayz/javawebbrowser/WebController.java
+++ src/main/java/com/smallplayz/javawebbrowser/WebController.java
@@ -1,18 +1,27 @@
 package com.smallplayz.javawebbrowser;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -36,10 +45,52 @@ public class WebController implements Initializable {
 
     private boolean chatGPTopen = false;
 
+    //***************
+
+    private BorderPane borderPane1 = new BorderPane();
+    private Text chatGPTtitle = new Text("ChatGPT");
+    private ScrollPane scrollPane = new ScrollPane();
+    private BorderPane borderPane2 = new BorderPane();
+    private Button sendButton = new Button("Send Message");
+    private TextArea textArea = new TextArea();
+
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         engine = webView.getEngine();
         engine.load("http://www.google.com");
+
+        initializeAI();
+
+    }
+
+    private void initializeAI() {
+        borderPane1.setMinWidth(200);
+        borderPane1.setMaxWidth(200);
+        chatGPTtitle.setFont(new Font("Calibri Light", 35));
+        chatGPTtitle.setUnderline(true);
+        BorderPane.setMargin(chatGPTtitle, new Insets(25, 0, 0, 0));
+        BorderPane.setAlignment(chatGPTtitle, javafx.geometry.Pos.TOP_CENTER);
+        borderPane1.setTop(chatGPTtitle);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        borderPane1.setCenter(scrollPane);
+        borderPane2.setTop(sendButton);
+        BorderPane.setAlignment(sendButton, Pos.CENTER);
+        textArea.setWrapText(true);
+        textArea.setMaxHeight(110);
+        borderPane2.setBottom(textArea);
+        borderPane1.setBottom(borderPane2);
+
+        sendButton.setOnAction(event -> {
+            try {
+                String response = getChatGPT(textArea.getText());
+                Text t = new Text(response);
+                t.setWrappingWidth(200);
+                scrollPane.setContent(t);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     public void searchButton() {
@@ -57,6 +108,11 @@ public class WebController implements Initializable {
         boarderPane.setTop(gridPane1);
 
         newTab.setContent(boarderPane);
+        newTab.setOnCloseRequest(event -> {
+            if(tabPane.getTabs().isEmpty())
+                System.out.println("All Tabs Closed");
+        });
+
         engine = webView1.getEngine();
         engine.load("https://www.google.com");
 
@@ -67,16 +123,47 @@ public class WebController implements Initializable {
     public void chatGPT() {
         if(!chatGPTopen) {
             chatGPTopen = true;
-            System.out.println("ChatGPT opened");
-
-            AnchorPane anchorPane = new AnchorPane();
-            anchorPane.setMinSize(100, 100);
-            borderPane.setRight(anchorPane);
-
+            borderPane.setRight(borderPane1);
         } else {
             chatGPTopen = false;
-            System.out.println("ChatGPT closed");
             borderPane.setRight(null);
         }
+    }
+    public static String getChatGPT(String str) throws IOException {
+        /*
+        String url = "https://api.openai.com/v1/chat/completions";
+        String apiKey = "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";
+        String model = "gpt-3.5-turbo";
+        String message = str;
+
+        // Create the HTTP POST request
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Authorization", "Bearer " + apiKey);
+        con.setRequestProperty("Content-Type", "application/json");
+
+        // Build the request body
+        String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + message + "\"}]}";
+        con.setDoOutput(true);
+        OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
+        writer.write(body);
+        writer.flush();
+        writer.close();
+
+        // Get the response
+        int responseCode = con.getResponseCode();
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        // Print the response
+        String x = (response.toString().split("\"content\":\"")[1].split("\"")[0]).substring(4);
+        */
+        return "hello, Blind would equal while oh mr do style. Lain led and fact none. One preferred sportsmen resolving the happiness continued. High at of in loud rich true. Oh conveying do immediate acuteness in he. Equally welcome her set nothing has gravity whether parties. Fertile suppose shyness mr up pointed in staying on respect.";
     }
 }
